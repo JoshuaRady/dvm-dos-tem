3d8fc225e9624b0d0dce89831722ec8931fb7f32
Joshua Rady <joshuarady@Steppin-Razor.local>
2024-06-12 10:49:25 -0400

    Continue flushing out psuedocode.

--- src/FW_Interface.cpp
+++ src/FW_Interface.cpp
@@ -24,7 +24,7 @@ void RevisedFire(WildFire* wf)//The name will definitely change.
 {
 
   //Determine the fuel model matching the location's CMT:
-  //fm = GetMatchingFuelModel(CMTnumber)
+  FuelModel fm = GetMatchingFuelModel(CMTnumber)
 
   //Determine the surface fuels from the model vegetation and soil states:
   //The fuels must be estimated from:
@@ -39,26 +39,41 @@ void RevisedFire(WildFire* wf)//The name will definitely change.
   //Update the fuel masses from their default values:
   //fm.w_o_ij[0] = ...
   
+  
+  //De we calculate the fuel bed depth?
+  
+  
   //Gather weather conditions:
-  //Temp, humidity, wind, slope, ...
   double tempAir = wf->edall.d_atms.ta;//Daily air temp (at surface).
+  //Curten (daily if not hourly) (relative) humidity is needed.  A recent time history of
   //double humidity = ?????
   //Wind speed (m/min) at midflame height is approximated as 2m wind speed.  We need a sub-daily value ~ daily value?
   //double windSpeed = ?????;//Wind speed does not seem to be available.
   //The wildfire object contains the slope.
   double slope = wf->slope;//What at the units?  May have to convert to fractional slope.
+  //Shortwave radiation may be needed for the moisture calculations.
+  
   
   //Fuel moisture must be calculated.
-  //The 1 and 10 hour values could probably be estimated from a daily value, though we have to
-  //assume a time of day.  We can superimpose a diurnal cycle.  The longer equilibrating fuels will
-  //need some way of estimating the humidity value for the recent past.
-  //Days since last rain is available in wf->edall.d_atms.
+  FuelMoisture theFuelMoisture = CalculateDeadFuelMoisture()
+  //fm.M_f_ij[0] = theFuelMoisture.XXXXX
   
   
   //Feed fuels and weather conditions into surface fire models:
-  //First to Rothermel & Albini
+  //First to Rothermel & Albini.  We use the calculations to get some component values.
+  //This interface is under development.  It takes a fuel model (and attendant data) and returns the
+  //calculation details.
+  //FM = SAV_ij, w_o_ij, M_x_1, M_f_ij, h_ij, S_T_ij, S_e_ij, rho_p_ij, liveDead
+  RAData raData = SpreadRateRothermelAlbini_Het(fm,
+                                     fuelBedDepth?????,//fuelBedDepth
+                                     windSpeed,//U
+                                     slope,//slopeSteepness
+                                     false,//useWindLimit
+                                     Metric,//units
+                                     FALSE)//debug
   
   //Use the fire energy flux into the soil along with the fuel model as input into Burnup:
+  //raData.XXXXX
   
   //Use the energy flux from the aboveground fire into the soil surface (RA + Burnup + crown) and
   //the fire air temp (Burnup fire environmental temperature?) as input to the ground fire model:
@@ -72,7 +87,10 @@ void RevisedFire(WildFire* wf)//The name will definitely change.
 
 }
 
-/* Determine the fuel model matching a given community type:*/
+/* Determine the fuel model matching a given community type:
+
+ * Note: FuelModel does not yet exist and is being developed elsewhere!
+ */
 FuelModel GetMatchingFuelModel(int cmt)
 {
   //Get the number of the fuel model from the crosswalk in the parameter files.
@@ -83,6 +101,42 @@ FuelModel GetMatchingFuelModel(int cmt)
   //fuelModel = GetFuelModel(fuelModelNumber);
   
   return fuelModel;
+}
+
+/* Calculate fuel moisture based on recent weather:
+*
+* This is a fire science dependent function and as such should probably be moved into the FW library.
+* Here the emphasis is on determining the available inputs we can pass in.
+* An alternative to calculating this at the time od fire would be to make fuel moisture a constantly
+* calculated state.  This would moke more sense if litter existed as a distinct stock as well.
+
+The 1 and 10 hour values could probably be estimated from a daily value, though we have to
+assume a time of day.  We can superimpose a diurnal cycle.  The longer equilibrating fuels will
+need some way of estimating the humidity value for the recent past.
+Days since last rain is available in wf->edall.d_atms.
+
+The Nelson and Fosberg models should be the first to consider.
+These models calculate moistures for ideal hour classes, which have matching diameters.  We could
+better inform the moistures with the actual SAV's diameters.
+Note: consider adding diameteres to the fuel model class!!!!!
+
+Inputs:
+- Humidity, temp, recent precip?
+
+Output:
+A fuel moisture object seems like a compact way to return the moisture values.
+Note: FuelMoisture does not currently exist!
+
+What about live fuel moisture?
+
+ */
+FuelMoisture CalculateDeadFuelMoisture()
+{
+  FuelMoisture dfmc;//FMC = dead fuel moisture content.
+  
+  //...
+  
+  return fmc;
 }
 
 /** Simulate ground fire returning or updating the burn depth...
