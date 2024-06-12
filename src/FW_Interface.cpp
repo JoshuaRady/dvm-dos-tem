d22b0270866668bd3dbcb1fd7cdabb92436809b9
Joshua Rady <joshuarady@Steppin-Razor.local>
2024-06-12 09:44:00 -0400

    Start work on outlining interface fucntions in psuedocode.

--- src/FW_Interface.cpp
+++ src/FW_Interface.cpp
@@ -36,15 +36,23 @@ void RevisedFire(WildFire* wf)//The name will definitely change.
   //Previous or current cfall could be used to help estimate this but some assumptions still need
   //to be made.
   
+  //Update the fuel masses from their default values:
+  //fm.w_o_ij[0] = ...
   
   //Gather weather conditions:
   //Temp, humidity, wind, slope, ...
-  
+  double tempAir = wf->edall.d_atms.ta;//Daily air temp (at surface).
+  //double humidity = ?????
+  //Wind speed (m/min) at midflame height is approximated as 2m wind speed.  We need a sub-daily value ~ daily value?
+  //double windSpeed = ?????;//Wind speed does not seem to be available.
+  //The wildfire object contains the slope.
+  double slope = wf->slope;//What at the units?  May have to convert to fractional slope.
   
   //Fuel moisture must be calculated.
   //The 1 and 10 hour values could probably be estimated from a daily value, though we have to
   //assume a time of day.  We can superimpose a diurnal cycle.  The longer equilibrating fuels will
   //need some way of estimating the humidity value for the recent past.
+  //Days since last rain is available in wf->edall.d_atms.
   
   
   //Feed fuels and weather conditions into surface fire models:
@@ -52,9 +60,48 @@ void RevisedFire(WildFire* wf)//The name will definitely change.
   
   //Use the fire energy flux into the soil along with the fuel model as input into Burnup:
   
-  //Use the energy flux into the soil (RA + Burnup) of the surface fire and the fire air temp
-  //(Burnup fire environmental temperature?) as input to the ground fire model:
+  //Use the energy flux from the aboveground fire into the soil surface (RA + Burnup + crown) and
+  //the fire air temp (Burnup fire environmental temperature?) as input to the ground fire model:
   //...
   
+  //Also pass in the soil profile conditions...
+  
+  //SimulateGroundFire();
+  //wf->fd->fire_soid.burnthick = burnDepth;
+  //Include messaging of original code?????
 
+}
+
+/* Determine the fuel model matching a given community type:*/
+FuelModel GetMatchingFuelModel(int cmt)
+{
+  //Get the number of the fuel model from the crosswalk in the parameter files.
+  //This crosswalk needs to be made!!!!!
+  int fuelModelNumber = 1;//Temporarily hardwired
+  
+  //Get the fuel model data:
+  //fuelModel = GetFuelModel(fuelModelNumber);
+  
+  return fuelModel;
+}
+
+/** Simulate ground fire returning or updating the burn depth...
+ *
+
+Burnup produces energy over time so it may be better to link the calculations?
+
+ */
+void SimulateGroundFire()
+{
+  double burnDepth = 0;
+  
+  //Calculate if the energy output is sufficiency to ignite the surface layer.
+  //If not record that ignition failed and return.
+  
+  //Otherwise continue to calculate progressive smoldering downward.
+  //This is the same problem of dying and heating to combustion as we move down.  However, we can
+  //safely assume that the fire will not continue if we reach mineral soil, bedrock, permafrost, or
+  //the water table.
+  
+  //return burnDepth;
 }
