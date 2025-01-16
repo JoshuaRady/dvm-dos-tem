ec0484809d7a77285f051860e17b62708bdb1eea
Joshua Rady <joshuarady@Steppin-Razor.local>
2025-01-16 10:47:12 -0500

    Clean up FW_Interface.h.

--- include/FW_Interface.h
+++ include/FW_Interface.h
@@ -14,25 +14,13 @@
 #ifndef FW_INTERFACE_H
 #define FW_INTERFACE_H
 
-//#include "Cohort.h"
 #include "FireweedRAFireSpread.h"
 
-//void RevisedFire(const Cohort& thisCohort, const ModelData& md, int monthIndex);//Move to WildFire.
-
 int GetMatchingFuelModel(int cmt);
-//void CohortStatesToFuelLoading(const Cohort& thisCohort, FuelModel& fm, bool treatMossAsDead);//Move to WildFire.
 void GetDeadFuelSizeDistribution(const FuelModel& fm, std::vector <double>& distribSAVs,
                                  std::vector <double>& distribWts);
-//bool IsShrub(const Cohort& thisCohort, int pftIdx);
 bool IsShrub(const int cmtNumber, const int pftIdx);
-
 void CalculateFuelBedDepth(FuelModel& fm, bool dynamic = true);
-
-//double GetMidflameWindSpeed(const Cohort& thisCohort);//Move to WildFire.
-
-//std::vector <double> CalculateFuelMoisture(const Cohort& thisCohort, const ModelData& md,
-//                                           const FuelModel& fm, int monthIndex);//Move to WildFire.
-
 void SimulateSurfaceCombustion(const FuelModel& fm, SpreadCalcs raData, double tempAir, double windSpeed);
 double SimulateGroundFire();
 
