c0d5b4c102e872a36a9aae770b86944b57d837e7
Joshua Rady <joshuarady@Steppin-Razor.local>
2025-09-10 13:13:58 -0400

    Rename fire_gf_d_max -> fire_gf_smolder_PD.

--- config/config.js
+++ config/config.js
@@ -164,8 +164,8 @@
         "fire_heat_frac_to_soil": 0.1,
         //The fraction of heat lost from the soil:
         "fire_gf_heat_loss_factor": 0.83,
-        //The maximum depth of heat transfer for the heat transfer approximation (cm):
-        "fire_gf_d_max": 5.0,
+        //The maximum heat penetration depth for the smoldering heat transfer approximation (cm):
+        "fire_gf_smolder_PD": 5.0,
         //The soil layer thickness for the simulation (cm):
         "fire_gf_layer_thickness": 1.0,
         //A temporaty parameter to force fuel model choice:
--- include/ModelData.h
+++ include/ModelData.h
@@ -135,7 +135,7 @@ public:
   bool fire_calculate_delta;//Calculate the fuel bed depth, AKA delta, (true) or use a fixed depth (false).
   double fire_heat_frac_to_soil;//The fraction of aboveground fire heat that enters the soil.
   double fire_gf_heat_loss_factor;//The fraction of heat lost from the soil
-  double fire_gf_d_max;//The maximum depth of heat transfer for the heat transfer approximation (cm).
+  double fire_gf_smolder_PD;//The maximum heat penetration depth for the smoldering heat transfer approximation (cm).
   double fire_gf_layer_thickness;//The soil layer thickness for the simulation (cm)
   int fire_temp_fm;//A temporaty parameter to force fuel model choice.
   // FW_MOD_END.
