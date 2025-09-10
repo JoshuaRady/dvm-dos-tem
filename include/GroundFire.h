c85394d854087ebaba70a617a7e2c3bfc48cae08
Joshua Rady <joshuarady@Steppin-Razor.local>
2025-09-10 12:54:59 -0400

    Sync with mother repository at 3b55794. Missed file.

--- include/GroundFire.h
+++ include/GroundFire.h
@@ -17,9 +17,12 @@
 #include "GFProfile.h"
 
 double SoilHeatOfCombustion(const double inorganicPct, const double organicHOC = 21.39148);//FW_PARAM
-double SoilHeatSink(const double moistureContentPct, const double T_int, const double T_ig, const double c_s);
+double EffectiveSmolderingHOC(const double inorganicPct);
+double SoilHeatSink(const double moistureContentPct, const double t_int, const double t_ig, const double c_s);
 std::vector <double> HeatDistributionLinear(const int numLayers);
 double DominoGroundFire(GFProfile& soilCol, const double fireHeatInput = 0.0,//With no heat input nothing will burn.
-                        const double heatLossFactor = 0.83, const double d_max = 5.0);
+                        const double heatLossFactor = 0.83, const double surfacePD = 1.0,
+                        const double smolderPD = 5.0, const int surfaceTM = 0,
+                        const int smolderTM = 1);
 
 #endif //FW_GROUNDFIRE_H
